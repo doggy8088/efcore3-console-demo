1728e897230ef2add564ea776de5f67fa59e558a
Will <doggy.huang@gmail.com>
2020-05-17 15:12:01 +0800

    示範 CRUD 寫法

--- Program.cs
+++ Program.cs
@@ -1,28 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace efc3
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<BlogContext>();
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=BlogDB;Integrated Security=true;");
 
             using(var db = new BlogContext(optionsBuilder.Options))
             {
-                // db.Database.EnsureCreated();
-
                 db.Blogs.Add(new Blog()
                 {
                     Rating = 5,
-                    Url = "https://blog.miniasp.com"
+                    Url = "https://www.miniasp.com"
                 });
 
+                var blog = db.Blogs.Find(1);
+                blog.Rating = 4;
+                db.SaveChanges();
+
+                var all = await db.Blogs.ToListAsync();
+
+                foreach (var item in all)
+                {
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.Rating);
+                    Console.WriteLine(item.Url);
+                    Console.WriteLine();
+                }
+
+                var blog_del = db.Blogs.Find(2);
+                db.Blogs.Remove(blog_del);
                 db.SaveChanges();
             }
-            Console.WriteLine("Hello World!");
         }
     }
 }
