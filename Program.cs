dd1f59a745f41afe16740fba1ac80e4e22f410af
Will <doggy.huang@gmail.com>
2020-05-17 13:44:47 +0800

    建立專案範本 dotnet new console -n efc3 && cd efc3
    安裝 EF Core 3.1 的 NuGet 套件
    dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 3.1.4
    dotnet add package Microsoft.EntityFrameworkCore.Design --version 3.1.4
    dotnet add package Microsoft.EntityFrameworkCore.Tools --version 3.1.4

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace efc3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
